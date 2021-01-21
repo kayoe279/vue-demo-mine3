cf1e2ea4624a6406979203597102ec7efecdcbcc
关灯请闭眼 <963981058@qq.com>
2021-01-21 11:48:23 +0800

    修改

--- src/utils/vueGlobal.js
+++ src/utils/vueGlobal.js
@@ -1,245 +1,33 @@
+/** 全局方法 */
 import Vue from 'vue';
-import router from '@/router/index.js';
-
-// upload
-
-// validate
-
-// upload url
-// Vue.prototype.$uploadUrl=`${BASE_URL}api/admin/upload`;
-// Vue.prototype.$uploadUrl=`http://yacht.test/api/admin/upload`;
-// Vue.prototype.$uploadUrl=`${BASE_URL}api/admin/upload?upload_type=banner`;
-// Vue.prototype.$uploadUrl=`http://yacht.test/api/admin/upload?upload_type=banner`;
-
-// role
-Vue.prototype.$getRole=function(){
-  return window.sessionStorage.getItem('ROLE');
-}
-
-// judgeRole
-Vue.prototype.$judgeRole=function(role='admin'){
-  return Vue.prototype.$getRole()==role;
-}
-
-// getRandomTagType
-Vue.prototype.$getRandomTagType=function(){
-  let types=['success','info','warning','danger'];
-  let index=Math.round(Math.random()*(types.length));
-
-  return types[index];
-}
-
-// removeArticle
-Vue.prototype.$removeArticle=function(id,cb){
-  let msg='确定要删除吗?';
-
-  this.$confirm(msg, '提示', {
-    type: 'warning'
-  }).then(() => {
-    this.$ajax("delete",`articles/${id}`).then(res=>{
-      cb&&cb();
-      this.$message({
-        message: '文章删除成功',
-        type: 'success'
-      });
-    });
-  }).catch(() => {
-
-  });
-}
-
-Vue.prototype.$removeYachtGroup=function(id,cb){
-  let msg='确定要删除吗?';
-
-  this.$confirm(msg, '提示', {
-    type: 'warning'
-  }).then(() => {
-    this.$ajax("delete",`admin/moments/${id}`).then(res=>{
-      cb&&cb();
-      this.$message({
-        message: '删除成功',
-        type: 'success'
-      });
-    });
-  }).catch(() => {
-
-  });
-}
-
-Vue.prototype.$updateYachtGroup=function(id,status,cb){
-  let data={
-    id:id,
-    status:status,
-  };
-
-  this.$ajax("put",`admin/moments/${id}`,data).then(res=>{
-    cb();
-    this.$message.success("操作成功");
-  });
-}
-
-// toUpdateArticle
-Vue.prototype.$toUpdateArticle=function(id){
-  router.push(`/article/media-info/article-show/${id}`);
-}
-
-Vue.prototype.$toUpdateBrand=function(id){
-  router.push(`/brand/show/${id}`);
-}
-
-Vue.prototype.$toUpdateBrandYacht=function(id){
-  router.push(`/brand/yacht-show/${id}/0`);
-}
-
-Vue.prototype.$toUpdateWharf=function(id){
-  router.push(`/wharf/show/${id}`);
-}
-
-Vue.prototype.$toAddBrandYacht=function(id){
-  router.push(`/brand/yacht-show/0/${id}`);
-}
-
-Vue.prototype.$toUpdateYachtSold=function(id){
-  router.push(`/yacht-sold/show/${id}`);
-}
-
-Vue.prototype.$toYachtGroup=function(id){
-  router.push(`/yacht-group/show/${id}`);
-}
-
-Vue.prototype.$toImportCatchData=function(id){
-  router.push({name:"catch-data-edit",params:{id:id}});
-}
-
-
-// switchStatus
-Vue.prototype.$switchState=function(uri,val,cb,column='active'){
-    console.log(column);
-  let data={
-      active:val
-  };
-  if(column == 'status'){
-    data = {
-      status:val
-    }
-  }
-  if(column == 'is_top'){
-      data = {
-          is_top:val
-      }
-  }
-  console.log(data);
-  this.$ajax("put",uri,data).then(res=>{
-    cb&&cb();
-    this.$message.success(res.data.message);
-  });
-}
-
-// $switchActive
-Vue.prototype.$switchActive=function(uri,val,cb){
-    let data={
-        active:val
-    };
-    this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-    }).then(() => {
-        this.$ajax("delete",uri,data).then(res=>{
-            cb&&cb();
-            data = res.data;
-            if (data.status_code == 0) {
-                this.$message({
-                    message: data.message,
-                    type: 'success'
-                });
-            } else {
-                this.$message({
-                    message: data.message,
-                    type: 'error'
-                });
-
-            }
-        });
-    })
-};
-
-Vue.prototype.$switchArticleState=function(id,val,cb){
-  let data={
-    id:id,
-    type:"active",
-    active:val
-  };
-
-  this.$ajax("put",`articles/${id}`,data).then(res=>{
-    cb&&cb();
-    this.$message.success("状态更新成功");
-  });
-}
-Vue.prototype.$switchArticleTop=function(id,val,cb){
-  let data={
-    is_top:val,
-  };
-
-  this.$ajax("put",`articles/${id}`,data).then(res=>{
-    cb&&cb();
-    this.$message.success("状态更新成功");
-  });
-}
-
-Vue.prototype.$switchBadgeState=function(id,val,cb){
-  let data={
-    id:id,
-    badge_status:val,
-  };
-
-  this.$ajax("put",`admin/catch-data/${id}`,data).then(res=>{
-    cb&&cb();
-    this.$message.success("状态更新成功");
-  });
-}
-
-// showImage
-Vue.prototype.$showImage=function(src,e,dialog){
-  let imgW=e.target.naturalWidth+40;
-  let winW=document.documentElement.clientWidth*.9;
-
-  dialog.src=src;
-  dialog.width=imgW>winW?`${winW}px`:`${imgW}px`;
-  dialog.visible=true;
-}
 
 //excel导出数据
 Vue.prototype.$exportExcel = function (name) {
-    var wb = XLSX.utils.table_to_book(document.querySelector('#out-table'))
-    var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' })
-    try {
-        FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }),name+ '.xlsx')
-    } catch (e) { if (typeof console !== 'undefined') console.log(e, wbout) }
-    return wbout
+  var wb = XLSX.utils.table_to_book(document.querySelector('#out-table'))
+  var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' })
+  try {
+    FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), name + '.xlsx')
+  } catch (e) { if (typeof console !== 'undefined') console.log(e, wbout) }
+  return wbout
 }
 
-//改变状态
-Vue.prototype.$changeStatus = function(cb){
-    cb&&cb();
-}
 //开始时间不能大于结束时间
-Vue.prototype.$timeValid = function (start,end) {
-    if(start > end){
-      this.$message({
-          type:'warning',
-          message:'开始时间不能大于结束时间'
-      })
-      return false;
-    }
-    return true;
+Vue.prototype.$timeValid = function (start, end) {
+  if (start > end) {
+    this.$message({
+      type: 'warning',
+      message: '开始时间不能大于结束时间'
+    })
+    return false;
+  }
+  return true;
 }
 
 //重置表单
-Vue.prototype.resetForm = function (refName){
+Vue.prototype.resetForm = function (refName) {
   if (this.$refs[refName]) {
-		this.$refs[refName].resetFields();
-	}
+    this.$refs[refName].resetFields();
+  }
 }
 
 // 统一处理
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,230 +1,222 @@
 <template>
-  <div id="hello">
-    <header></header>
-    <div class="buttons">
-      <div
-        class="item-btn"
-        :class="'bg-'+item.color"
-        v-for="(item, index) in navigationList"
-        :key="index"
-        @click="navToView(item.url)"
-      >{{item.name}}</div>
-    </div>
-    <main>
-      <div class="subButton" @click="change" style="font-size:16px;">数量加1</div>
-      <div class="subButton" @click="clearList">重置count</div>
-      <div class="count-box">
-        <span class="count">数量:{{count}}</span>
-        <span>showPubSub值:{{showPubSub}}</span>
-      </div>
-      <div class="box">
-        <div>VUEX控制</div>
-        <button @click="sortAge">按岁数排序</button>
-        <ul>
-          <li v-for="(item, index) in List" :key="index">
-            <span>姓名:{{item.name}},年龄:{{item.age}}</span>
-          </li>
-        </ul>
-      </div>
-    </main>
-  </div>
+	<div id="hello">
+		<header></header>
+		<div class="buttons">
+			<div class="item-btn" :class="'bg-'+item.color" v-for="(item, index) in navigationList" :key="index" @click="navToView(item.url)">{{item.name}}</div>
+		</div>
+		<main>
+			<div class="subButton" @click="change">数量加1</div>
+			<div class="subButton" @click="clearList">重置count</div>
+			<div class="count-box">
+				<span class="count">数量:{{count}}</span>
+				<span>showPubSub值:{{showPubSub}}</span>
+			</div>
+			<div class="box">
+				<div>VUEX控制</div>
+				<button @click="sortAge">按岁数排序</button>
+				<ul>
+					<li v-for="(item, index) in List" :key="index">
+						<span>姓名:{{item.name}},年龄:{{item.age}}</span>
+					</li>
+				</ul>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapActions } from "vuex";
-import PubSub from "pubsub-js";
+import { mapGetters, mapMutations, mapActions } from 'vuex';
+import PubSub from 'pubsub-js';
 export default {
-  name: "Index",
-  data() {
-    return {
-      show2: false,
-      navigationList: [
-        // { name: "第一个page", color: "red", url: "/first" },
-        // { name: "第二个page", color: "orange", url: "/second" },
-        // { name: "接口页", color: "yellow", url: "/testPage" },
-        // { name: "瀑布流布局页面", color: "olive", url: "/waterfall" },
-        // { name: "vant组件测试", color: "green", url: "/vantTest" },
-        { name: "自定义Popup组件", color: "cyan", url: "/popupTest" },
-        { name: "自定义Loading", color: "blue", url: "/loadingTest" },
-        { name: "自定义Toast", color: "purple", url: "/toastTest" },
-        { name: "自定义Modal", color: "pink", url: "/modalTest" },
-        { name: "Calendar日历", color: "green", url: "/calendarTest" }
-      ],
-      picker: [
-        { label: "<b style='color:red'>拍照</b>", type: "creame" },
-        { label: "从相册选择", type: "selectPhoto" }
-      ],
-      allList: [
-        { name: "xiaoming", age: 13, weight: 20 },
-        { name: "liming", age: 23, weight: 21 },
-        { name: "zhangming", age: 8, weight: 66 },
-        { name: "liming", age: 55, weight: 21 },
-        { name: "lisa", age: 72, weight: 21 },
-        { name: "maliya", age: 336, weight: 21 },
-        { name: "zhangsan", age: 596, weight: 21 },
-        { name: "wangwu", age: 1352, weight: 21 },
-        { name: "kangkang", age: 11, weight: 21 },
-        { name: "datiedan", age: 2, weight: 21 }
-      ],
-      showPubSub: null
-    };
-  },
-  computed: {
-    ...mapGetters({
-      title: "getTitle",
-      count: "getCount",
-      List: "getList"
-    })
-  },
-  methods: {
-    toPage(page) {
-      // 三种跳转方式
-      this.$router.push({ path: "/" + page });
-      // this.$router.push({name:page.substr(0,1).toUpperCase() + page.substr(1)})
-      // this.$router.push('/' + page)
-    },
-    navToView(url) {
-      this.$router.push({ path: url });
-    },
-    change() {
-      if (this.count === this.allList.length) return;
-      this.item = this.allList[this.count];
-      this.invokeList(this.item); // 或者  this.$store.dispatch("invokeList", this.item);
-      this.increment(1); // 或者 this.$store.commit("increment", 1);
-    },
-    ...mapMutations(["increment"]),
-    ...mapActions(["clearList", "invokeList"]),
-    // 排序
-    sortAge() {
-      this.show2 = true;
-      if (this.List.length === 0) return;
-      this.List.sort(function(a, b) {
-        return a.age - b.age;
-      });
-    },
-    showPicker() {
-      this.show2 = true;
-    }
-  },
-  beforeDestroy() {
-    // 销毁订阅
-    PubSub.unsubscribe("changeState");
-  },
-  mounted() {
-    let _this = this;
-    // this.$coverLoading();
-    PubSub.subscribe("changeState", (event, data) => {
-      _this.showPubSub = data;
-      console.log(data);
-    });
-  }
+	name: 'Index',
+	data() {
+		return {
+			show2: false,
+			navigationList: [
+				// { name: "第一个page", color: "red", url: "/first" },
+				// { name: "第二个page", color: "orange", url: "/second" },
+				// { name: "接口页", color: "yellow", url: "/testPage" },
+				// { name: "瀑布流布局页面", color: "olive", url: "/waterfall" },
+				// { name: "vant组件测试", color: "green", url: "/vantTest" },
+				{ name: '自定义Popup组件', color: 'cyan', url: '/popupTest' },
+				{ name: '自定义Loading', color: 'blue', url: '/loadingTest' },
+				{ name: '自定义Toast', color: 'purple', url: '/toastTest' },
+				{ name: '自定义Modal', color: 'pink', url: '/modalTest' },
+				{ name: 'Calendar日历', color: 'green', url: '/calendarTest' },
+			],
+			picker: [
+				{ label: "<b style='color:red'>拍照</b>", type: 'creame' },
+				{ label: '从相册选择', type: 'selectPhoto' },
+			],
+			allList: [
+				{ name: 'xiaoming', age: 13, weight: 20 },
+				{ name: 'liming', age: 23, weight: 21 },
+				{ name: 'zhangming', age: 8, weight: 66 },
+				{ name: 'liming', age: 55, weight: 21 },
+				{ name: 'lisa', age: 72, weight: 21 },
+				{ name: 'maliya', age: 336, weight: 21 },
+				{ name: 'zhangsan', age: 596, weight: 21 },
+				{ name: 'wangwu', age: 1352, weight: 21 },
+				{ name: 'kangkang', age: 11, weight: 21 },
+				{ name: 'datiedan', age: 2, weight: 21 },
+			],
+			showPubSub: null,
+		};
+	},
+	computed: {
+		...mapGetters({
+			title: 'getTitle',
+			count: 'getCount',
+			List: 'getList',
+		}),
+	},
+	methods: {
+		toPage(page) {
+			// 三种跳转方式
+			this.$router.push({ path: '/' + page });
+			// this.$router.push({name:page.substr(0,1).toUpperCase() + page.substr(1)})
+			// this.$router.push('/' + page)
+		},
+		navToView(url) {
+			this.$router.push({ path: url });
+		},
+		change() {
+			if (this.count === this.allList.length) return;
+			this.item = this.allList[this.count];
+			this.invokeList(this.item); // 或者  this.$store.dispatch("invokeList", this.item);
+			this.increment(1); // 或者 this.$store.commit("increment", 1);
+		},
+		...mapMutations(['increment']),
+		...mapActions(['clearList', 'invokeList']),
+		// 排序
+		sortAge() {
+			this.show2 = true;
+			if (this.List.length === 0) return;
+			this.List.sort((a, b) => a.age - b.age);
+		},
+		showPicker() {
+			this.show2 = true;
+		},
+	},
+	beforeDestroy() {
+		// 销毁订阅
+		PubSub.unsubscribe('changeState');
+	},
+	mounted() {
+		let _this = this;
+		// this.$coverLoading();
+		PubSub.subscribe('changeState', (event, data) => {
+			_this.showPubSub = data;
+			console.log(data);
+		});
+	},
 };
 </script>
 
 <style lang="scss">
 #hello {
-  height: 100%;
-  background-color: #fff;
-  padding-bottom: 25px;
-  background: url("../assets/img/bg-skin.png") no-repeat;
-  background-size: 100% 100%;
-  overflow-y: auto;
-  header {
-    width: 100%;
-    height: 50px;
-    background-size: 100% 100%;
-  }
-  .buttons {
-    padding: 15px;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    .item-btn {
-      width: 48%;
-      height: 100px;
-      line-height: 100px;
-      border-radius: 5px;
-      padding: 0 5px;
-      margin: 0 0 20px 0;
-      font-size: 16px;
-      background-image: url("https://image.weilanwl.com/color2.0/cardBg.png");
-      background-size: cover;
-      background-position: center;
-      position: relative;
-      z-index: 100;
-      color: #fff;
-      &::after {
-        content: "";
-        position: absolute;
-        z-index: -1;
-        background-color: inherit;
-        width: 100%;
-        height: 100%;
-        left: 0;
-        bottom: -12%;
-        border-radius: 5px;
-        opacity: 0.2;
-        transform: scale(0.9, 0.9);
-      }
-    }
-  }
-  main {
-    width: 100%;
-    margin-top: 20px;
-    padding: 15px;
-    .count-box {
-      margin-bottom: 15px;
-    }
-    .subButton {
-      width: 100%;
-      height: 45px;
-      line-height: 45px;
-      border-radius: 5px;
-      padding: 0 5px;
-      margin: 0 0 10px 0rem;
-      font-size: 18px;
-      background: #8dc63f;
-      color: #fff;
-      box-shadow: rgba(26, 26, 26, 0.2);
-    }
-    .box {
-      width: 100%;
-      border-radius: 6px;
-      background-image: url("https://image.weilanwl.com/color2.0/cardBg.png");
-      background-size: cover;
-      background-position: center;
-      position: relative;
-      z-index: 1;
-      background-color: #1cbbb4;
-      padding: 15px;
-      color: #fff;
-      &::after {
-        content: "";
-        position: absolute;
-        z-index: -1;
-        background-color: inherit;
-        width: 100%;
-        height: 100%;
-        left: 0;
-        bottom: -10%;
-        border-radius: 5px;
-        opacity: 0.2;
-        transform: scale(0.9, 0.9);
-      }
-    }
-    ul li {
-      width: 100%;
-      padding: 7.5px 0;
-    }
-    button {
-      width: 100%;
-      padding: 10px 0;
-      border-radius: 6px;
-      margin-top: 10px;
-      background-color: #fff;
-      color: #1cbbb4;
-      border: none;
-    }
-  }
+	height: 100%;
+	background-color: #fff;
+	padding-bottom: 25px;
+	background: url('../assets/img/bg-skin.png') no-repeat;
+	background-size: 100% 100%;
+	overflow-y: auto;
+	header {
+		width: 100%;
+		height: 50px;
+		background-size: 100% 100%;
+	}
+	.buttons {
+		padding: 15px;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		.item-btn {
+			width: 48%;
+			height: 100px;
+			line-height: 100px;
+			border-radius: 5px;
+			padding: 0 5px;
+			margin: 0 0 20px 0;
+			font-size: 16px;
+			background-image: url('https://image.weilanwl.com/color2.0/cardBg.png');
+			background-size: cover;
+			background-position: center;
+			position: relative;
+			z-index: 100;
+			color: #fff;
+			&::after {
+				content: '';
+				position: absolute;
+				z-index: -1;
+				background-color: inherit;
+				width: 100%;
+				height: 100%;
+				left: 0;
+				bottom: -12%;
+				border-radius: 5px;
+				opacity: 0.2;
+				transform: scale(0.9, 0.9);
+			}
+		}
+	}
+	main {
+		width: 100%;
+		margin-top: 20px;
+		padding: 15px;
+		.count-box {
+			margin-bottom: 15px;
+		}
+		.subButton {
+			width: 100%;
+			height: 45px;
+			line-height: 45px;
+			border-radius: 5px;
+			padding: 0 5px;
+			margin: 0 0 10px 0rem;
+			font-size: 18px;
+			background: #8dc63f;
+			color: #fff;
+			box-shadow: rgba(26, 26, 26, 0.2);
+		}
+		.box {
+			width: 100%;
+			border-radius: 6px;
+			background-image: url('https://image.weilanwl.com/color2.0/cardBg.png');
+			background-size: cover;
+			background-position: center;
+			position: relative;
+			z-index: 1;
+			background-color: #1cbbb4;
+			padding: 15px;
+			color: #fff;
+			&::after {
+				content: '';
+				position: absolute;
+				z-index: -1;
+				background-color: inherit;
+				width: 100%;
+				height: 100%;
+				left: 0;
+				bottom: -10%;
+				border-radius: 5px;
+				opacity: 0.2;
+				transform: scale(0.9, 0.9);
+			}
+		}
+		ul li {
+			width: 100%;
+			padding: 7.5px 0;
+		}
+		button {
+			width: 100%;
+			padding: 10px 0;
+			border-radius: 6px;
+			margin-top: 10px;
+			background-color: #fff;
+			color: #1cbbb4;
+			border: none;
+		}
+	}
 }
 </style>
